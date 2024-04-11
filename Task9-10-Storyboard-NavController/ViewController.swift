67eb6b2ce4a3088f7532a72beaa9d6344d5174fb
studyingProjects <148430835+studyingProjects@users.noreply.github.com>
2024-04-11 14:11:01 +0300

    Initial Commit

---
+++ Task9-10-Storyboard-NavController/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Task9-10-Storyboard-NavController
+//
+//  Created by Andrei Shpartou on 11/04/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
