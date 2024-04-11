4b4fe216e6917a3d1b9ca4c419500f2eee8548dd
studyingProjects <148430835+studyingProjects@users.noreply.github.com>
2024-04-11 15:54:56 +0300

    implement FoodList scene

---
+++ Task9-10-Storyboard-NavController/Scenes/FoodList/FoodListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  FoodListViewController.swift
+//  Task9-10-Storyboard-NavController
+//
+//  Created by Andrei Shpartou on 11/04/2024.
+//
+
+import UIKit
+
+class FoodListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        navigationItem.setHidesBackButton(true, animated: false)
+    }
+}
