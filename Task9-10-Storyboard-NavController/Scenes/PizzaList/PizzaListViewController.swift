9e7e05cbfb199792a604cc9cd46265e8c00c8ade
studyingProjects <148430835+studyingProjects@users.noreply.github.com>
2024-04-12 14:21:30 +0300

    implement PizzaList scene

---
+++ Task9-10-Storyboard-NavController/Scenes/PizzaList/PizzaListViewController.swift
@@ -0,0 +1,16 @@
+//
+//  PizzaListViewController.swift
+//  Task9-10-Storyboard-NavController
+//
+//  Created by Andrei Shpartou on 11/04/2024.
+//
+
+import UIKit
+
+class PizzaListViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        navigationController?.navigationBar.tintColor = .black
+    }
+}
