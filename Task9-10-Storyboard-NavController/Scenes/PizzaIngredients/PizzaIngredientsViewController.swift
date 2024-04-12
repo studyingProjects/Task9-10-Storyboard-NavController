49242e6897075c30cfa2e39997bd3349a5c69c02
studyingProjects <148430835+studyingProjects@users.noreply.github.com>
2024-04-12 14:42:10 +0300

    implement pizza ingredients layout

---
+++ Task9-10-Storyboard-NavController/Scenes/PizzaIngredients/PizzaIngredientsViewController.swift
@@ -0,0 +1,10 @@
+//
+//  PizzaIngredientsViewController.swift
+//  Task9-10-Storyboard-NavController
+//
+//  Created by Andrei Shpartou on 12/04/2024.
+//
+
+import UIKit
+
+class PizzaIngredientsViewController: UIViewController {}
